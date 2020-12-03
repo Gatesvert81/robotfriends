1cb53e5b08e85165d325c9c86081299c5c9fe19c
Gatesvert81 <63714591+Gatesvert81@users.noreply.github.com>
2020-12-03 01:11:35 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import CardList from './CardList.js';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
 
 class App extends Component {
     constructor() {
-        super()1
+        super()
         this.state = {
             robots: [],
-            serchfield: ''
+            searchfield: ''
         }
     }
 
@@ -20,15 +19,29 @@ class App extends Component {
             .then(users =>  {this.setState({ robots: users })});
     }
 
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+      }
+
+    
+    
     render() {
-        return (
-            <div className='tc' >
-                <h1 className='f2' > RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={this.state.robots} />
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+          return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+          
+        })
+        return !robots.length ?
+          <h1>Loading</h1> :
+          (
+            <div className='tc'>
+              <h1 className='f1'>RoboFriends</h1>
+              <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
             </div>
-        );
-    }   
+          );
+      }
+
 }
 
 export default App; 
